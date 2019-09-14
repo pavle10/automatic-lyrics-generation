9c9ba0aa6a93faacd7034c3614cf1ed379f837eb
Pavle Markovic <pavlemark@hotmail.rs>
2019-09-14 16:05:12 +0200

    Refactoring rnn model

--- rnn.py
+++ rnn.py
@@ -11,94 +11,149 @@ LAYERS_NUM = 2
 LAYER_SIZE = [256, 256, 256]
 
 
-def main():
-    dataset = hlp.get_dataset()
-    dataset = dataset[:10000]
-    chars = sorted(list(set(dataset)))
-    dataset_size = len(dataset)
-    vocab_size = len(chars)
-    idx_to_char = {i: ch for i, ch in enumerate(chars)}
-    char_to_idx = {ch: i for i, ch in enumerate(chars)}
+class RnnModel:
 
-    # Make training examples
-    print("Making training examples...")
-    X = list()
-    Y = list()
+    def __init__(self):
+        print("Initializing dataset and vocabulary...")
 
-    for i in range(0, dataset_size - SEQ_LENGTH):
-        input_seq = dataset[i:i+SEQ_LENGTH]
-        label_seq = dataset[i+SEQ_LENGTH]
-        X.append([char_to_idx[ch] for ch in input_seq])
-        Y.append(char_to_idx[label_seq])
+        self.dataset = hlp.get_dataset()[:100000]
+        self.dataset_size = len(self.dataset)
+        self.vocab = sorted(list(set(self.dataset)))
+        self.vocab_size = len(self.vocab)
+        self.idx_to_char = {i: ch for i, ch in enumerate(self.vocab)}
+        self.char_to_idx = {ch: i for i, ch in enumerate(self.vocab)}
+        self.examples = list()
+        self.labels = list()
+        self.num_of_examples = 0
+        self.model = None
 
-    n_examples = len(X)
+        print("Finished with initialization!")
 
-    print(f"Finished with making training examples with total number {n_examples}")
+    def make_examples(self):
+        # Make training examples
+        print("Making training examples...")
+        self.examples = list()
+        self.labels = list()
 
-    # Reshape examples
-    X = np.reshape(X, (n_examples, SEQ_LENGTH, 1))
-    # Normalize input data
-    X = X / float(vocab_size)
-    # One-hot encode output data
-    Y = np_utils.to_categorical(Y)
+        for i in range(0, self.dataset_size - SEQ_LENGTH):
+            example_seq = self.dataset[i:i + SEQ_LENGTH]
+            label_seq = self.dataset[i + SEQ_LENGTH]
+            self.examples.append([self.char_to_idx[ch] for ch in example_seq])
+            self.labels.append(self.char_to_idx[label_seq])
 
-    # Make the model
-    print("Start making the model")
-    model = Sequential()
+        self.num_of_examples = len(self.examples)
 
-    model.add(LSTM(LAYER_SIZE[0], input_shape=(X.shape[1], X.shape[2]), return_sequences=True))  # input layer
+        print(f"Finished with making training examples with total number {self.num_of_examples}")
 
-    for i in range(1, LAYERS_NUM):
-        model.add(LSTM(LAYER_SIZE[i], return_sequences=True))  # hidden layers
+    def make_model(self):
+        x, y = self.prepare_examples()
 
-    model.add(Flatten())  # flatten last hidden layer
+        # Make the model
+        print("Start making the model")
+        self.model = Sequential()
 
-    model.add(Dense(Y.shape[1]))
-    model.add(Activation("softmax"))
-    model.compile(loss="categorical_crossentropy", optimizer="adam")
+        self.model.add(LSTM(LAYER_SIZE[0], input_shape=(x.shape[1], x.shape[2]), return_sequences=True))  # input layer
 
-    model.summary()
+        for i in range(1, LAYERS_NUM):
+            self.model.add(LSTM(LAYER_SIZE[i], return_sequences=True))  # hidden layers
 
-    print("Finished with making the model")
+        self.model.add(Flatten())  # flatten last hidden layer
 
-    # Configure checkpoint through callbacks
-    checkpoint_name = "Weights-LSTM-improvement-{epoch:03d}-{loss:.5f}.hdf5"
-    checkpoint = ModelCheckpoint(checkpoint_name, monitor="loss", verbose=1, save_best_only=True, mode="min")
-    callbacks_list = [checkpoint]
+        self.model.add(Dense(y.shape[1]))
+        self.model.add(Activation("softmax"))
+        self.model.compile(loss="categorical_crossentropy", optimizer="adam")
 
-    # Do the training
-    model_params = {"epochs": 30,
-                    "batch_size": 128,
-                    "callbacks": callbacks_list,
-                    "verbose": 1,
-                    "validation_split": 0.2,
-                    "validation_data": None,
-                    "shuffle": True,
-                    "initial_epoch": 0,
-                    "steps_per_epoch": None,
-                    "validation_steps": None}
+        self.model.summary()
 
-    model.fit(X,
-              Y,
-              epochs=model_params["epochs"],
-              batch_size=model_params["batch_size"],
-              callbacks=model_params["callbacks"],
-              verbose=model_params["verbose"],
-              validation_split=model_params["validation_split"],
-              validation_data=model_params["validation_data"],
-              shuffle=model_params["shuffle"],
-              initial_epoch=model_params["initial_epoch"],
-              steps_per_epoch=model_params["steps_per_epoch"],
-              validation_steps=model_params["validation_steps"])
+        print("Finished with making the model")
 
+    def train(self):
+        # Configure checkpoint through callbacks
+        checkpoint_name = "Weights-LSTM-improvement-{epoch:03d}-{loss:.5f}.hdf5"
+        checkpoint = ModelCheckpoint(checkpoint_name, monitor="loss", verbose=1, save_best_only=True, mode="min")
+        callbacks_list = [checkpoint]
 
-def generate_lyrics():
-    # Load the weights
-    weights_file = "./Weights-LSTM-improvement-003-3.09039.hdf5"
+        x, y = self.prepare_examples()
 
-    # Generate lyrics
+        # Do the training
+        model_params = {"epochs": 30,
+                        "batch_size": 128,
+                        "callbacks": callbacks_list,
+                        "verbose": 1,
+                        "validation_split": 0.2,
+                        "validation_data": None,
+                        "shuffle": True,
+                        "initial_epoch": 0,
+                        "steps_per_epoch": None,
+                        "validation_steps": None}
+
+        self.model.fit(x,
+                       y,
+                       epochs=model_params["epochs"],
+                       batch_size=model_params["batch_size"],
+                       callbacks=model_params["callbacks"],
+                       verbose=model_params["verbose"],
+                       validation_split=model_params["validation_split"],
+                       validation_data=model_params["validation_data"],
+                       shuffle=model_params["shuffle"],
+                       initial_epoch=model_params["initial_epoch"],
+                       steps_per_epoch=model_params["steps_per_epoch"],
+                       validation_steps=model_params["validation_steps"])
+
+    def generate_lyrics(self):
+        # Load the weights
+        weights_file = "./models/model_01/Weights-LSTM-improvement-030-0.00771.hdf5"
+        self.model.load_weights(weights_file)
+        self.model.compile(loss="categorical_crossentropy", optimizer="adam")
+
+        # Generate lyrics
+        start = np.random.randint(0, self.num_of_examples-1)
+        pattern = self.examples[start]
+        print("Seed:")
+        print(self.reverse_to_chars(pattern))
+
+        num_of_chars = 300
+        output = list()
+
+        for i in range(num_of_chars):
+            x = np.reshape(pattern, (1, len(pattern), 1))
+            x = x / float(self.vocab_size)
+            prediction = self.model.predict(x, verbose=0)
+            idx = np.argmax(prediction)
+            res = self.idx_to_char[idx]
+            output.append(res)
+            pattern.append(res)
+            pattern = pattern[1:len(pattern)]
+
+        print("Output:")
+        print("".join(output))
+        print("-----")
+        print("Finished with generation!")
+
+    def prepare_examples(self):
+        # Reshape examples
+        x = np.reshape(self.examples, (self.num_of_examples, SEQ_LENGTH, 1))
+        # Normalize input data
+        x = x / float(self.vocab_size)
+        # One-hot encode output data
+        y = np_utils.to_categorical(self.labels)
+
+        return x, y
+
+    def reverse_to_chars(self, l):
+        if type(l) is int:
+            return self.idx_to_char[l]
+
+        temp = list()
+        for idx in l:
+            temp.append(self.idx_to_char[idx])
+
+        return "".join(temp)
 
 
 if __name__ == '__main__':
-    main()
-    # generate_lyrics()
+
+    model = RnnModel()
+    model.make_examples()
+    model.make_model()
+    model.generate_lyrics()
